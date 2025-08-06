aa747345150b53983aa6e8bf18004105b63fc1d9
StudioG14 <ociepa-piotr@wp.pl>
2025-08-06 10:57:38 +0100

    Auto commit: 10-08-2025_06%e (~2041 MB incl. .git)

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Zwiększ wersję przy każdej zmianie - BŁĘDY JS FIX
-const CACHE_NAME = 'v34-mobile-pwa-render-functions';
+const CACHE_NAME = 'v35-mobile-main-ui-section';
 const urlsToCache = [
   './',
   'index.html',
